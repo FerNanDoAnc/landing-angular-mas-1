1336a155e566b141aae21119485dac042636978f
Fernando Ancajima <fernando.ancajima99@gmail.com>
2024-05-31 15:49:13 -0500

    A_fix_max

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ export const routes: Routes = [
   {path:'',
     loadChildren:()=>import('./web/principal/principal.module').then(mod=>mod.PrincipalModule)
   },
-  {
-    path: '**',
-    redirectTo: ''
-  }
+  // {
+  //   path: '**',
+  //   redirectTo: ''
+  // }
 ];
